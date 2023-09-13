e8fd0f96df380e02fe19fff84131b59047193eb1
Kyle Baumgarten <BaumDev@proton.me>
2023-09-12 21:37:26 -0400

    waybar

--- swayland/waybar/checkbothbatteries.py
+++ swayland/waybar/checkbothbatteries.py
@@ -1,7 +1,5 @@
 #!/bin/python
 
-import os
-
 #get the battery status from  sys/class/power_supply/BAT1/energy_now
 bat0 = open('/sys/class/power_supply/BAT0/energy_now', 'r')
 bat1 = open('/sys/class/power_supply/BAT1/energy_now', 'r')
@@ -10,5 +8,10 @@ bat1full = open('/sys/class/power_supply/BAT1/energy_full', 'r')
 total = (int(bat0.read()) + int(bat1.read())) / (int(bat0full.read()) + int(bat1full.read())) * 100
 #take to 2 decimal places
 total = round(total, 2)
+#print(total)
+#return the value in json such as {"text": "$text", "tooltip": "$tooltip", "class": "$class", "percentage": $percentage }
 
-print (total)
+result = '{"text": "' + str(total) + '%", "tooltip": "Battery ' + str(total) + '%", "class": "battery", "percentage": ' + str(total) + '}'
+
+print(result)
+#print (total)
