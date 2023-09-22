055c42d7b3b5f995536b2be785df2dc7386fd45f
Kyle Baumgarten <BaumDev@proton.me>
2023-09-21 23:47:21 -0400

    battery script, dual batteries t480

--- swayland/waybar/checkbothbatteries.py
+++ swayland/waybar/checkbothbatteries.py
@@ -11,7 +11,17 @@ total = round(total, 2)
 #print(total)
 #return the value in json such as {"text": "$text", "tooltip": "$tooltip", "class": "$class", "percentage": $percentage }
 
-result = '{"text": "' + str(total) + '%", "tooltip": "Battery ' + str(total) + '%", "class": "battery", "percentage": ' + str(total) + '}'
+discharging=None
+discharging0= open('/sys/class/power_supply/BAT0/status', 'r')
+discharging1= open('/sys/class/power_supply/BAT1/status', 'r')
+if discharging0.read() == "Discharging\n" or discharging1.read() == "Discharging\n":
+    discharging="Discharging"
+else:
+    discharging="Charging"
+
+
+
+result = '{"text": "' + str(total) + '%", "tooltip": "Battery ' + str(total) + '%, '+discharging+'", "class": "battery", "percentage": ' + str(total) + '}'
 
 print(result)
 #print (total)
