ad927e32c7638ab497a259d799e4f0fdbdf439d0
Kyle Baumgarten <BaumDev@proton.me>
2025-01-25 12:29:57 -0500

    waybar config

--- swayland/waybar/checkbatswaybar.py
+++ swayland/waybar/checkbatswaybar.py
@@ -5,8 +5,7 @@ bat0 = open('/sys/class/power_supply/BAT0/energy_now', 'r')
 bat1 = open('/sys/class/power_supply/BAT1/energy_now', 'r')
 bat0full = open('/sys/class/power_supply/BAT0/energy_full', 'r')
 bat1full = open('/sys/class/power_supply/BAT1/energy_full', 'r')
-bat1full=int(bat1full.read())*.8
-total = (int(bat0.read()) + int(bat1.read())) / (int(bat0full.read()) + int(bat1full)) * 100
+total = (int(bat0.read()) + int(bat1.read())) / (int(bat0full.read()) + int(bat1full.read())) * 100
 #take to 2 decimal places
 total = round(total, 2)
 #print(total)
@@ -22,9 +21,8 @@ else:
 
 
 
-#result = '{"text": "' + str(total) + '%", "tooltip": "Battery ' + str(total) + '%, '+discharging+'", "class": "battery", "percentage": ' + str(total) + '}'
-result = '{"percentage": ' + str(total) + ',"discharging":"'+discharging+'", "text":"'+str(total)+'","tooltip": "Battery ' + str(total) + '%, '+discharging+'" }'
 
+result=str(total)
 
 print(result)
 #print (total)
