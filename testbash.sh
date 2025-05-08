5805f1e5ea44c06cb77c5ea217db9e309200e137
Baum <120348740+TreeGardenDev@users.noreply.github.com>
2025-05-08 16:09:40 -0400

    Add files via upload

--- testbash.sh
+++ testbash.sh
@@ -17,9 +17,11 @@ fd -e desktop . /usr/share/applications ~/.local/share/applications 2>/dev/null 
     exec=$(grep -m 1 '^Exec=' "$desktop_file" | cut -d '=' -f 2 | sed 's/%.//g' | awk '{print $1}') # Extract the first word of the Exec command
     no_display=$(grep -m 1 '^NoDisplay=' "$desktop_file" | cut -d '=' -f 2)
     terminal=$(grep -m 1 '^Terminal=' "$desktop_file" | cut -d '=' -f 2)
+    startupnotify=$(grep -m 1 '^StartupNotify=' "$desktop_file" | cut -d '=' -f 2)
+
 
     # Filter out non-invokable apps
-    if [ -n "$name" ] && [ -n "$exec" ] && [ "$no_display" != "true" ] && [ "$terminal" != "true" ] && command -v "$exec" >/dev/null 2>&1; then
+    if [ -n "$name" ] && [ -n "$exec" ] && [ "$startupnotify" != "false" ] && [ "$no_display" != "true" ] && [ "$terminal" != "true" ] && command -v "$exec" >/dev/null 2>&1; then
         echo "$name|$exec"
     fi
 done | fzf --border=rounded --border-label="Fuzzy Launcher" --layout=reverse-list --delimiter="|" --with-nth=1 | cut -d "|" -f 2 | xargs -r sh -c
